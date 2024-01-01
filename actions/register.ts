875b7fd50eda432b6662178045e86bfb4bacadd5
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-02 00:17:02 +0600

    create an user in neon database

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
+import bcrypt from 'bcrypt'; // if this cause problem use bcryptjs
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields' };
   }
 
-  return { success: 'Emain Sent!' };
+  const { email, password, name } = validateFields.data;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  return { success: 'User Created!' };
 };
