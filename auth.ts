9bbf53d401c18e2c0029bdb896d99e939df56234
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-03 20:58:04 +0600

    implemented basic google and github login functionality

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from './lib/db';
