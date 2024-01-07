b7fc204738a2a4f9fe4a03469275324ba1e6b125
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-07 19:28:53 +0600

    implemented admin page and protected admin route by role

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { auth } from '@/auth';
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
           />
         </head>
         <body className={inter.className}>{children}</body>
+        <Toaster />
       </html>
     </SessionProvider>
   );
